2d8ab739a9f58601cd8ebb07221d0145a18dbaff
chenjuan21 <cj1q2w3e4r>
2019-03-13 07:49:41 +0800

    注释

--- cj_kaikeba/vue-cli/demo/src/main.js
+++ cj_kaikeba/vue-cli/demo/src/main.js
@@ -4,7 +4,13 @@ import Vue from 'vue'
 // import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false  //开发和生产环境的配置
+
+//配置axios
+import Axios from 'axios'
+//配置公共URL
+Axios.defaults.baseURL = 'https://www.sinya.online/api/'
+Vue.prototype.$axios = Axios
 
 /* eslint-disable no-new */
 new Vue({
--- cj_kaikeba/vue-cli/demo/src/router/index.js
+++ cj_kaikeba/vue-cli/demo/src/router/index.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 import login from '@/login'
 import reg from '@/reg'
 
-Vue.use(Router)
+Vue.use(Router)  //注册全局组件router-view router-linke  挂载在Vue.prototype.$router || $route 未来所有组件中的this对象，就具备该属性，所有的this其实就是Vue的子类对象
 
 export default new Router({
   routes: [
