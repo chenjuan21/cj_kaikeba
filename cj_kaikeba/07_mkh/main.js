08db1d1c944b11261e2e699d68ef6d648921e885
chenjuan21 <cj1q2w3e4r>
2019-03-15 07:43:17 +0800

    模块化

---
+++ cj_kaikeba/07_mkh/main.js
@@ -0,0 +1,18 @@
+//整个程序的入口
+import Vue from './vue.js'
+//import App from './App.js'
+// import {num1,num2} from './app.js';
+// console.log(num1,num2);
+
+import * as obj from './App.js'
+console.log(obj.num1);
+console.log(obj.num2);
+
+new Vue({
+    el:'#app',
+    components: {
+        // app:App
+        app:app.default
+    },
+    template:'<app/>'
+})
