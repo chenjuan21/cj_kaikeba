9e2e3a588743b2cd7999437cfe2156f8d1de3ab9
986673182@qq.com <986673182@qq.com>
2019-03-15 15:14:09 +0800

    es6 模块化

--- cj_kaikeba/07_mkh/main.js
+++ cj_kaikeba/07_mkh/main.js
@@ -1,6 +1,6 @@
 //整个程序的入口
 import Vue from './vue.js'
-//import App from './App.js'
+// import App from './App.js'
 // import {num1,num2} from './app.js';
 // console.log(num1,num2);
 
@@ -11,8 +11,8 @@ console.log(obj.num2);
 new Vue({
     el:'#app',
     components: {
-        // app:App
-        app:app.default
+       // app:App
+       app:app.default
     },
     template:'<app/>'
 })
