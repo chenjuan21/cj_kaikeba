ae7b9a1dc36158f9463387c625c44b5464f836a5
chenjuan21 <cj1q2w3e4r>
2019-04-08 07:23:30 +0800

    fdsf

---
+++ cj_kaikeba/vue-cli/project/src/page/goreg.vue
@@ -0,0 +1,81 @@
+<template>
+<div>
+        <section>
+		<div class="ydc-content-slide ydc-body">
+			<div class="ydc-page-content" style="overflow:hidden">
+				<div class="ydc-reg-title">
+					<ul class="clearfix">
+						<li class="ydc-reg-step active">
+							<em></em>
+							<h3>登录邮箱账号</h3>
+						</li>
+						<li class="ydc-reg-step">
+							<em></em>
+							<h3>选择帐号类别</h3>
+						</li>
+						<li class="ydc-reg-step">
+							<em></em>
+							<h3>填写媒体信息</h3>
+						</li>
+					</ul>
+				</div>
+				<div class="ydc-reg-form clearfix">
+					<form action="">
+						<div class="ydc-reg-form-group clearfix">
+							<label>邮箱账号:</label>
+							<div class="ydc-reg-form-input">
+								<input type="text" id="loginEmail" name="username" class="ydc-form-control" autocomplete="off" placeholder="账号通行证/邮箱">
+							</div>
+							<div class="ydc-reg-form-text">
+								<p>请填写账号邮箱，作为登录帐号。<br>没有账号邮箱？ <a href="javascript:;">立即注册>></a></p>
+							</div>
+						</div>
+						<div class="ydc-reg-form-group clearfix">
+							<label>密码:</label>
+							<div class="ydc-reg-form-input">
+								<input type="password" id="psd" name="username" class="ydc-form-control" autocomplete="off" placeholder="密码">
+							</div>
+							<div class="ydc-reg-form-text">
+								<p><a href="javascript:;">忘记密码？</a></p>
+							</div>
+						</div>
+						<div class="ydc-reg-form-group">
+							<div class="ydc-reg-form-button">
+								<a class="btn" type="submit" href="go-reg-username.html" >登录</a>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="ydc-right-banner" style="margin-top:80px;">
+					<div class="slideshow-container">
+						<a href="https://xihazahuopu.taobao.com/" target="_blank" class="mySlides fade">
+							<img src="admin/images/ad/ad1.jpg" style="width:100%">
+						</a>
+						<a href="https://weibo.com/525135676" target="_blank" class="mySlides fade">
+							<img src="admin/images/ad/ad2.jpg" style="width:100%">
+						</a>
+						<a href="http://www.a-ui.cn/" target="_blank" class="mySlides fade">
+							<img src="admin/images/ad/ad3.jpg" style="width:100%">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="ydc-footer">
+			<div>
+				<p>©2018 一点车版权所有About 公司简介  联系方法  招聘信息  客户服务  隐私政策  广告服务  网站地图  意见反馈  不良信息举报</p>
+			</div>
+		</div>
+	</section>
+</div>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
+
